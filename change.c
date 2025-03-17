2dd2d1f33578bf080d0a7b56b83160ec98f27685
BryceHardaway <bhardaway28@brophybroncos.org>
2025-03-17 21:01:20 +0000

    Finished

--- change.c
+++ change.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 
 int main(void){
-    //Work here
-    printf("hello");
+    
+    int amt;
+    int loop = 10;
+
+    while (loop == 10)
+    {
+        
+        printf("\nHow much change?(enter as a whole number) ");
+        scanf("%d", &amt);
+
+        if (amt < 100) {
+            break;
+        }
+        if (amt >= 100) {
+            printf("The amount you entered is above $1.00");
+        }
+    }
+    
 
 
+        int quarters = amt / 25;
+        amt %= 25;
+        int nickles = amt / 5;
+        amt %= 5;
+        int dimes = amt / 10;
+        amt %= 10;
+        int pennies = amt;
+        amt %= amt;
+
+        int totalcoins = pennies + quarters + nickles + dimes;
+
+        printf("Quarters: %d\n", quarters);
+        printf("Dimes: %d\n", dimes);
+        printf("Nickles: %d\n", nickles);
+        printf("Pennies: %d\n", pennies);
+        printf("Total coins: %d\n", totalcoins);
 
     return 0;
 }
